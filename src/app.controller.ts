8b139546564202f674b6d5393b4bd39bcf4be998
idirbfs <85136127+idirbfs@users.noreply.github.com>
2023-11-20 14:25:05 +0100

    update home.hbs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,17 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Controller, Get, Render, Inject } from '@nestjs/common';
 import { AppService } from './app.service';
-import {Mots } from "./models/mots.model";
+import { Mot } from './models/mots.model';
+import { Document } from './models/documents.model';
 import { Sequelize } from 'sequelize-typescript';
-
+import { Occurrence } from './models/occurences.model';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService, @Inject('SEQUELIZE')
-  private sequelize: Sequelize,) {}
+  constructor(
+    private readonly appService: AppService,
+    @Inject('SEQUELIZE')
+    private sequelize: Sequelize,
+  ) {}
 
   // @Get()
   // getHello(): string {
@@ -16,7 +20,10 @@ export class AppController {
   @Render('home')
   @Get()
   async root() {
-    const words = await Mots.findAll();
+    const words = await Occurrence.findAll({ include: [Mot, Document] });
+    console.log('====================================');
+    console.log(words);
+    console.log('====================================');
     return { words };
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TextProcessingModule } from './text-processing/text-processing.module';
 import { VisualisationModule } from './visualisation/visualisation.module';
+import { databaseProviders } from './database/database.provider';
 
 @Module({
   imports: [TextProcessingModule, VisualisationModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ...databaseProviders],
 })
 export class AppModule {}
